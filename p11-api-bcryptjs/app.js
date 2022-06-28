5207a8c9e15923ada1484bab76aee84a24daae22
Théo Ligório <theoligorio04@gmail.com>
2022-06-27 21:43:29 -0300

    PASTAS DE BACKEND

--- p11-api-bcryptjs/app.js
+++ p11-api-bcryptjs/app.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const app = express();
 require('dotenv').config();
 const User = require('./models/User');
+const sendMail = require('./providers/mailProvider')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -52,23 +53,49 @@ app.get('/users/:id', async (req,res) => {
     }
 });
 
-app.post("/user" , async (req, res) => {
-    // const {name, email, gender, password} = req.body;
+app.post("/user", async (req, res) => {
 
-    var dados = req.body
-    console.log(dados)
+    var dados = req.body;
     dados.password = await bcrypt.hash(dados.password, 8);
-    console.log(dados.password);
+    let email = dados.email;
+    let name = dados.name;
+    let gender = dados.gender;
 
     await User.create(dados)
-    .then( () => {
+    .then( ()=>{
+        /* enviar e-mail */
+        let to = email;
+        let cc = '';
+        var htmlbody = "";
+        htmlbody += '<div style="background-color:#000; margin-bottom:150px;">';
+        htmlbody += '<div style="margin-top:150px;">';
+        htmlbody += '<p style="color:#fff; font-weight:bold;margin-top:50px;">';
+        htmlbody += 'Olá {name},';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff; font-style:italic;margin-top:50px;">';
+        htmlbody += 'Sua conta foi criada com sucesso!';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff;margin-top:50px;">';
+        htmlbody += 'Seu login é o seu email: {email}';
+        htmlbody += '</p>';
+        htmlbody += '<p style="color:#fff;margin-top:50px;">';
+        htmlbody += 'Sexo: {gender}';
+        htmlbody += '</p>';
+        htmlbody += '</div>';
+        htmlbody += '</div>';
+        htmlbody = htmlbody.replace('{name}', name);
+        htmlbody = htmlbody.replace('{email}', email);
+        htmlbody = htmlbody.replace('{gender}', gender);
+        /* ************* */
+        sendMail(to, cc, 'Sua conta foi criada com sucesso!', htmlbody);
+
         return res.json({
             erro: false,
-            mensagem: "Usuário cadastrado com sucesso!!"
+            mensagem: 'Usuário cadastrado com sucesso!'
         });
-    }).catch ((err) => {
+    }).catch( (err)=>{
         return res.status(400).json({
-            erro: true,
+            erro:true,
             mensagem: `Erro: Usuário não cadastrado... ${err}`
         })
     })
--- p11-api-bcryptjs/providers/mailProvider.js
+++ p11-api-bcryptjs/providers/mailProvider.js
@@ -1,10 +1,10 @@
-module.exports = function sendEmail(to, cc, subject, html){
-    const nodemailer = require ('nodemailer');
+module.exports = function sendMail(to,cc,subject,html){
+    const nodemailer = require('nodemailer');
 
     const smtpTransport = nodemailer.createTransport({
         host: process.env.SMTP_SERVER,
         port: parseInt(process.env.SMTP_PORT),
-        secure: true,
+        // secure: true,
         auth: {
             user: process.env.SMTP_USERNAMEACCOUNT,
             pass: process.env.SMTP_PASSWORD
@@ -12,17 +12,17 @@ module.exports = function sendEmail(to, cc, subject, html){
     })
 
     const message = {
-        from: process.env.SMTP_USERNAMEACOOUNT,
+        from: 'contato@jsfernando.com',
         to,
         cc,
-        bcc: process.env.SMTP_USERNAMEACOOUNT,
+        bcc: process.env.SMTP_USERNAMEACCOUNT,
         subject,
         html
     }
 
-    smtpTransport.sendEmail(message, (err, res) => {
-        if (err) {
-            console.log(`Erro ao enviar o email: ${err}`)
+    smtpTransport.sendMail(message, (err, res) =>{
+        if(err){
+            console.log(`Erro ao enviar email: ${err}`);
         } else {
             console.log('Email enviado com sucesso!')
         }
